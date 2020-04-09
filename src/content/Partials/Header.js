4ba55e3286c30af6123853d146a0c8df4841a953
kennansalisbury <kennansalisbury@gmail.com>
2020-04-09 14:50:21 -0700

    FPO about page in place

--- src/content/Partials/Header.js
+++ src/content/Partials/Header.js
@@ -11,24 +11,47 @@ export const Header = props => {
     }
 
     if (!props.user) {
-        return (
-            <div className='header'>
-                <div className='header__title'>
-                    <p className='heading-two'>SewStrong</p>
+        if(props.headerType) {
+            if(props.headerType === 'about_contact') {
+                return (
+                    <div className='header'>
+                        <div className='header__title'>
+                            <p className='heading-two'>SewStrong</p>
+                        </div>
+                        <div className='header__nav'>
+                            <Link className='header__nav__link body-two' to='/'>Home</Link>
+                            <Link className="header__nav__link body-two" to="/about">About</Link>
+                            <Link className='header__nav__link body-two' to="/contact">Contact</Link>
+                        </div>
+                        <div className='header__auth'>
+                            <AuthLink text='SIGN UP' type='background-orange' setShowSignup={props.setShowSignup} />
+                            <AuthLink text='LOGIN' type='background-green' setShowLogin={props.setShowLogin} />
+                        </div>
+                    </div>
+                )
+            }
+        }
+        else {
+        
+            return (
+                <div className='header'>
+                    <div className='header__title'>
+                        <p className='heading-two'>SewStrong</p>
+                    </div>
+                    <div className='header__nav'>
+                        <Link className="header__nav__link body-two" to="/about">About</Link>
+                        <a className='header__nav__link body-two' href='#volunteer'>Volunteer</a>
+                        <a className='header__nav__link body-two' href='#order'>Order</a>
+                        <a className='header__nav__link body-two' href='#resources'>Resources</a>
+                        <Link className='header__nav__link body-two' to="/contact">Contact</Link>
+                    </div>
+                    <div className='header__auth'>
+                        <AuthLink text='SIGN UP' type='background-orange' setShowSignup={props.setShowSignup} />
+                        <AuthLink text='LOGIN' type='background-green' setShowLogin={props.setShowLogin} />
+                    </div>
                 </div>
-                <div className='header__nav'>
-                    <Link className="header__nav__link body-two" to="/about">About</Link>
-                    <a className='header__nav__link body-two' href='#volunteer'>Volunteer</a>
-                    <a className='header__nav__link body-two' href='#order'>Order</a>
-                    <a className='header__nav__link body-two' href='#resources'>Resources</a>
-                    <a className='header__nav__link body-two' href='#contact'>Contact</a>
-                </div>
-                <div className='header__auth'>
-                    <AuthLink text='SIGN UP' type='background-orange' setShowSignup={props.setShowSignup} />
-                    <AuthLink text='LOGIN' type='background-green' setShowLogin={props.setShowLogin} />
-                </div>
-            </div>
-        )
+            )
+        }
     } else {
         return (
             <div className='header'>
