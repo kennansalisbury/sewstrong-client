e8dfae5050ee41738cae2f1f29969f627502a364
kennansalisbury <kennansalisbury@gmail.com>
2020-04-10 15:31:52 -0700

    admin can update inventories

--- src/content/Dashboard/components/index.js
+++ src/content/Dashboard/components/index.js
@@ -2,5 +2,6 @@ import { AdminDash } from './AdminDash';
 import { AdminDashOrder } from './AdminDashOrder';
 import { AdminDashVol } from './AdminDashVol';
 import { Aside } from './Aside';
+import { InventoryUpdate } from './InventoryUpdate'
 
-export { AdminDash, AdminDashOrder, AdminDashVol, Aside };
+export { AdminDash, AdminDashOrder, AdminDashVol, Aside, InventoryUpdate };
