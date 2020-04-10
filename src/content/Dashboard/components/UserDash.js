7df5ac66cd25ab292dac24cebe6dc0f563ff7799
Gavin Callander <gavincallander@Gavins-MacBook-Pro.local>
2020-04-10 17:50:46 -0400

    scss update

--- src/content/Dashboard/components/UserDash.js
+++ src/content/Dashboard/components/UserDash.js
@@ -2,11 +2,13 @@ import React, { useState } from 'react';
 
 export const UserDash = props => {
 
-
+    
 
     return (
         <div className='dashboard__user'>
-            <div className='dashboard__user__'></div>
+            <div className='dashboard__user__'>
+
+            </div>
         </div>
     )
 };
